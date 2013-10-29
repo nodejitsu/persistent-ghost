5495645760a62c2017561f63d7078002880216e1
Swaagie <info@martijnswaagman.nl>
2013-10-29 17:11:58 +0100

    [minor] add casper to example config

--- config/config.example.js
+++ config/config.example.js
@@ -6,6 +6,15 @@
 exports.mongo = 'mongodb://localhost:27017/ghost';
 
 /**
+ * Which themes should be added to /themes, provide as { name: source }.
+ *
+ * @type {Object}
+ */
+exports.themes = {
+  casper: 'git://github.com/TryGhost/Casper.git'
+};
+
+/**
  * OPTIONAL: Provide e-mail service configuration, not providing these details will
  * disable password recovery, also a closeable warning is shown. Example: GMAIL
  *
